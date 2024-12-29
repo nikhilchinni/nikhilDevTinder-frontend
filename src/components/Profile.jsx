30e0bfac144048ed3376987733732b8562fe1d02
Chinni Nikhil <nikhilchinni06@gmail.com>
2024-12-29 20:07:34 +0530

    Edit Profile feature

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import EditProfile from './EditProfile'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const user = useSelector(store => store.user);
   return (
-    <div>test page</div>
+    user && (
+      <div>
+        <EditProfile user={user} />
+      </div>
+    )
   )
 }
 
